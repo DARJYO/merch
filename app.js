39b554c8e1dd4dfeae92294c04ff4d7596ce10b5
persadian <34051185+arishma108@users.noreply.github.com>
2026-04-22 16:53:09 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+const canvas = document.getElementById("hero-canvas")
+
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(
+75,
+window.innerWidth/window.innerHeight,
+0.1,
+1000
+)
+
+const renderer = new THREE.WebGLRenderer({canvas,alpha:true})
+
+renderer.setSize(window.innerWidth,window.innerHeight)
+
+camera.position.z = 3
+
+
+const geometry = new THREE.TorusKnotGeometry(1,0.3,120,16)
+
+const material = new THREE.MeshStandardMaterial({
+
+color:0x6cff4d,
+metalness:0.7,
+roughness:0.3,
+wireframe:true
+
+})
+
+const knot = new THREE.Mesh(geometry,material)
+
+scene.add(knot)
+
+
+const light = new THREE.PointLight(0xffffff,1)
+
+light.position.set(5,5,5)
+
+scene.add(light)
+
+
+function animate(){
+
+requestAnimationFrame(animate)
+
+knot.rotation.x += 0.003
+knot.rotation.y += 0.004
+
+renderer.render(scene,camera)
+
+}
+
+animate()
+
+
+window.addEventListener("resize",()=>{
+
+renderer.setSize(window.innerWidth,window.innerHeight)
+
+camera.aspect = window.innerWidth/window.innerHeight
+camera.updateProjectionMatrix()
+
+})
